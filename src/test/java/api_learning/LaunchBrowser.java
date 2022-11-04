626f11bf6c6a7481ee1882079e07acf746f79d3c
Le Thi Tra My <lethitramy@Les-MacBook-Pro.local>
2022-11-04 16:05:24 +0700

    lesson 16

---
+++ src/test/java/api_learning/LaunchBrowser.java
@@ -0,0 +1,34 @@
+package api_learning;
+
+import org.apache.commons.exec.OS;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String chromeDriverLocation;
+        if (OS.isFamilyMac()) {
+            chromeDriverLocation = "/src/test/resources/drivers/chromedriver";
+        } else if (OS.isFamilyWindows()) {
+            chromeDriverLocation = "\\src\\test\\resources\\drivers\\chromedriver.exe";
+        } else {
+            throw new RuntimeException("[ERR] Couldn't detect the OS ");
+        }
+        String chromeAbsoluteLocation = currentProjectLocation.concat(chromeDriverLocation);
+        System.setProperty("webdriver.chrome.driver", chromeAbsoluteLocation);
+        //Set up Chrome options
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognoto");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://learn.sdetpro.com");
+        //Debug
+        try {
+            Thread.sleep(3000);
+        } catch (Exception ignored) {
+        }
+        driver.quit();
+    }
+
+}
